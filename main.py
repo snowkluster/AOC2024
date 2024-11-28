5115f61b0889f4adbecdac9f6a22cc8cf0533e6d
snowkluster <rohankaushik1@outlook.com>
2024-11-28 14:57:51 +0530

    start subprocess bash script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+import subprocess
 import requests
 import datetime
 import sys
@@ -14,6 +15,7 @@ if (month != 12 or day > 25):
     print("Not december or day greater than 25, no advent of code")
     sys.exit()
 
+result = subprocess.run(['bash', 'example.sh'], text=True)
 page = requests.get(f"https://adventofcode.com/{year}/day/{day}")
 soup = BeautifulSoup(page.content, 'html.parser')
 article_content = soup.find('article', class_='day-desc')
