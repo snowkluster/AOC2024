e18aa4419d31ffcccc6387d73994eba2967f6f5a
snowkluster <rohankaushik1@outlook.com>
2024-11-28 12:41:06 +0530

    get the page content
    - Get Question and save to a markdown file
    - Get the input file and save to correct folder
    - Update gitignore and create requirements.txt for scrapper to work

--- input.sh
+++ input.sh
@@ -5,7 +5,7 @@ date_total=$(date +%d/%Y)
 day=$(echo "$date_total" | cut -d '/' -f1)
 year=$(echo "$date_total" | cut -d '/' -f2)
 
-if [[ $day -gt 25 ]];
+if [ $day -gt 25 ];
 then
 	echo "it's so over, maybe next year"
 	exit 0
