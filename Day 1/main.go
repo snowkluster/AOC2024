ef45cfa0a863a6ef86b429dbf5d5652912dfbbd7
snowkluster <rohankaushik1@outlook.com>
2024-12-01 13:40:08 +0530

    Day 1 Star 1

--- Day 1/main.go
+++ Day 1/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-  "fmt"
+	"bufio"
+	"bytes"
+	"fmt"
 	"os"
+  "math"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -11,9 +17,53 @@ func check(e error) {
     }
 }
 
+func day1s1(data []byte) (sum int){
+  var arr1 []int
+  var arr2 []int
+  scanner := bufio.NewScanner(bytes.NewReader(data))
+  for scanner.Scan() {
+    line := strings.Split(scanner.Text(), "   ")
+    int1, err := strconv.ParseInt(line[0] ,10, 32)
+    check(err)
+    int2, err := strconv.ParseInt(line[1] ,10, 32)
+    check(err)
+    arr1 = append(arr1, int(int1))
+    arr2 = append(arr2, int(int2))
+  }
+  sort.Ints(arr1)
+  sort.Ints(arr2)
+  for i:= range len(arr1) {
+      distance := math.Abs(float64(arr1[i] - arr2[i]))
+      sum += int(distance)
+  }
+  return
+}
+
+func day1s2(data []byte) (find int){
+  var arr1 []int
+  var arr2 []int
+  scanner := bufio.NewScanner(bytes.NewReader(data))
+  for scanner.Scan() {
+    line := strings.Split(scanner.Text(), "   ")
+    int1, err := strconv.ParseInt(line[0] ,10, 32)
+    check(err)
+    int2, err := strconv.ParseInt(line[1] ,10, 32)
+    check(err)
+    arr1 = append(arr1, int(int1))
+    arr2 = append(arr2, int(int2))
+  }
+  sort.Ints(arr1)
+  sort.Ints(arr2)
+  // Working on Second Star
+  return
+}
+
 func main() {
-	fmt.Println("Welcome to AOC 2024")
 	data, err := os.ReadFile("input.txt")
+  const prec = uint(100)
   check(err)
-  fmt.Print(string(data))
+  sum := day1s1(data)
+  find := day1s2(data)
+  fmt.Println(sum)
+  fmt.Println(find)
 }
